c5d57aeae19343754e58d611859e43587ac53272
Aditi <aditisuman5480@gmail.com>
2022-02-12 23:20:00 +0530

    count

---
+++ calculate_coin.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int amount;
+    cout << "Enter the amount " << endl;
+    cin >> amount;
+
+    int Rs100 , Rs50, Rs20, Rs1;
+
+    switch (1)
+    {
+    case 1:
+        Rs100 = amount / 100;
+        amount = amount % 100;
+        cout << "No of Rs 100 note is " << Rs100 << endl;
+
+    case 2:
+        Rs50 = amount / 50;
+        amount = amount % 50;
+        cout << "No of Rs 50 note is " << Rs50 << endl;
+
+    case 3:
+        Rs20 = amount / 20;
+        amount = amount % 20;
+        cout << "No of Rs 20 note is " << Rs20 << endl;
+
+    case 4:
+        Rs1 = amount / 1;
+        amount = amount % 1;
+        cout << "No of Rs 10 note is " << Rs1 << endl;
+    }
+    return 0;
+}
